3bbebadfd3233c034e036a63c07bd0019274e9bb
AnBapDan <dani.andrade@ua.pt>
2021-11-07 16:56:53 +0000

    :sparkles: Encryption Using AES done!

--- main.c
+++ main.c
@@ -6,7 +6,6 @@ typedef uint8_t initial[4][4];
 
 static initial keySchedule[10];
 
-
 //TODO remove and accept input
 
 static initial matrix = {
@@ -14,21 +13,6 @@ static initial matrix = {
         {0x43, 0x5a, 0x31, 0x37},
         {0xf6, 0x30, 0x98, 0x07},
         {0xa8, 0x8d, 0xa2, 0x34}
-};
-
-static initial test = {
-        {0x19, 0xa0, 0x9a, 0xe9},
-        {0x3d, 0xf4, 0xc6, 0xf8},
-        {0xe3, 0xe2, 0x8d, 0x48},
-        {0xbe, 0x2b, 0x2a, 0x08}
-};
-
-//TODO remove after Mixcolumns
-static initial columns = {
-        {0x04, 0xe0, 0x48, 0x28},
-        {0x66, 0xcb, 0xf8, 0x06},
-        {0x81, 0x19, 0xd3, 0x26},
-        {0xe5, 0x9a, 0x7a, 0x4c}
 };
 
 static initial key = {
@@ -48,7 +32,7 @@ static uint8_t s_box[16][16] = {
         {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75},
         {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84},
         {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf},
-        {0xd0, 0xef, 0xaa, 0xfd, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8},
+        {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8},
         {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2},
         {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73},
         {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb},
@@ -162,11 +146,11 @@ int createKeySchedule() {
     return 0;
 }
 
-int AddRoundKey() {
+int AddRoundKey(int round) {
     //TODO alterar test por matrix
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            test[j][i] ^= key[j][i];
+            matrix[j][i] ^= keySchedule[round][j][i];
         }
     }
 
@@ -179,9 +163,9 @@ int SubBytes() {
     for (int i = 0; i < 4; i++) {
 
         for (int j = 0; j < 4; j++) {
-            uint8_t x = (test[j][i] >> 4);
-            uint8_t y = (test[j][i] & 0x0f);
-            test[j][i] = s_box[x][y];
+            uint8_t x = (matrix[j][i] >> 4);
+            uint8_t y = (matrix[j][i] & 0x0f);
+            matrix[j][i] = s_box[x][y];
 
         }
 
@@ -193,80 +177,121 @@ int ShiftRows() {
     //TODO alterar test por matrix
     //Row 2
     uint8_t temp = 0;
-    temp = test[1][0];
+    temp = matrix[1][0];
     for (int i = 0; i <= 2; i++) {
-        test[1][i] = test[1][i + 1];
+        matrix[1][i] = matrix[1][i + 1];
     }
-    test[1][3] = temp;
+    matrix[1][3] = temp;
 
     //Row 3
-    temp = test[2][0];
-    test[2][0] = test[2][2];
-    test[2][2] = temp;
-    temp = test[2][1];
-    test[2][1] = test[2][3];
-    test[2][3] = temp;
+    temp = matrix[2][0];
+    matrix[2][0] = matrix[2][2];
+    matrix[2][2] = temp;
+    temp = matrix[2][1];
+    matrix[2][1] = matrix[2][3];
+    matrix[2][3] = temp;
 
     //Row 4
-    temp = test[3][3];
+    temp = matrix[3][3];
     for (int i = 3; i > 0; i--) {
-        uint8_t tmp = test[3][i - 1];
-        test[3][i] = tmp;
+        uint8_t tmp = matrix[3][i - 1];
+        matrix[3][i] = tmp;
     }
-    test[3][0] = temp;
+    matrix[3][0] = temp;
 
     return 0;
 }
 
-int MixColumns() {
-    //TODO this code is all wrong, need to implement galois field
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            test[j][i] = columns[j][i];
+
+
+//mixSingleColumn and MixColumns functions were retrieved from: https://github.com/amanske/aes-128/blob/master/aes.cpp
+
+/* Helper function for the mix columns step
+   Implementation taken from https://en.wikipedia.org/wiki/Rijndael_mix_columns#Implementation_example */
+void mixSingleColumn(unsigned char *r) {
+    unsigned char a[4];
+    unsigned char b[4];
+    unsigned char c;
+    unsigned char h;
+    /* The array 'a' is simply a copy of the input array 'r'
+     * The array 'b' is each element of the array 'a' multiplied by 2
+     * in Rijndael's Galois field
+     * a[n] ^ b[n] is element n multiplied by 3 in Rijndael's Galois field */
+    for (c = 0; c < 4; c++) {
+        a[c] = r[c];
+        /* h is 0xff if the high bit of r[c] is set, 0 otherwise */
+        h = (unsigned char) ((signed char) r[c]
+                >> 7); /* arithmetic right shift, thus shifting in either zeros or ones */
+        b[c] = r[c]
+                << 1; /* implicitly removes high bit because b[c] is an 8-bit char, so we xor by 0x1b and not 0x11b in the next line */
+        b[c] ^= 0x1B & h; /* Rijndael's Galois field */
+    }
+    r[0] = b[0] ^ a[3] ^ a[2] ^ b[1] ^ a[1]; /* 2 * a0 + a3 + a2 + 3 * a1 */
+    r[1] = b[1] ^ a[0] ^ a[3] ^ b[2] ^ a[2]; /* 2 * a1 + a0 + a3 + 3 * a2 */
+    r[2] = b[2] ^ a[1] ^ a[0] ^ b[3] ^ a[3]; /* 2 * a2 + a1 + a0 + 3 * a3 */
+    r[3] = b[3] ^ a[2] ^ a[1] ^ b[0] ^ a[0]; /* 2 * a3 + a2 + a1 + 3 * a0 */
+}
+
+/* Performs the mix columns step. Theory from: https://en.wikipedia.org/wiki/Advanced_Encryption_Standard#The_MixColumns_step */
+void MixColumns() {
+    unsigned char temp[4];
+
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            temp[j] = matrix[j][i]; //place the current state column in temp
+        }
+        mixSingleColumn(temp); //mix it using the wiki implementation
+        for (int j = 0; j < 4; ++j) {
+            matrix[j][i] = temp[j]; //when the column is mixed, place it back into the state
         }
     }
-    return 0;
 }
 
 int printTestSchedule() {
-    printf("%X %X %X %X \n", keySchedule[10][0][0], keySchedule[10][0][1], keySchedule[10][0][2], keySchedule[10][0][3]);
-    printf("%X %X %X %X \n", keySchedule[10][1][0], keySchedule[10][1][1], keySchedule[10][1][2], keySchedule[10][1][3]);
-    printf("%X %X %X %X \n", keySchedule[10][2][0], keySchedule[10][2][1], keySchedule[10][2][2], keySchedule[10][2][3]);
-    printf("%X %X %X %X \n\n", keySchedule[10][3][0], keySchedule[10][3][1], keySchedule[10][3][2], keySchedule[10][3][3]);
+    printf("%X %X %X %X \n", keySchedule[10][0][0], keySchedule[10][0][1], keySchedule[10][0][2],
+           keySchedule[10][0][3]);
+    printf("%X %X %X %X \n", keySchedule[10][1][0], keySchedule[10][1][1], keySchedule[10][1][2],
+           keySchedule[10][1][3]);
+    printf("%X %X %X %X \n", keySchedule[10][2][0], keySchedule[10][2][1], keySchedule[10][2][2],
+           keySchedule[10][2][3]);
+    printf("%X %X %X %X \n\n", keySchedule[10][3][0], keySchedule[10][3][1], keySchedule[10][3][2],
+           keySchedule[10][3][3]);
 }
 
 
 int printTest() {
-    printf("%X %X %X %X \n", test[0][0], test[0][1], test[0][2], test[0][3]);
-    printf("%X %X %X %X \n", test[1][0], test[1][1], test[1][2], test[1][3]);
-    printf("%X %X %X %X \n", test[2][0], test[2][1], test[2][2], test[2][3]);
-    printf("%X %X %X %X \n\n", test[3][0], test[3][1], test[3][2], test[3][3]);
+    printf("%X %X %X %X \n", matrix[0][0], matrix[0][1], matrix[0][2], matrix[0][3]);
+    printf("%X %X %X %X \n", matrix[1][0], matrix[1][1], matrix[1][2], matrix[1][3]);
+    printf("%X %X %X %X \n", matrix[2][0], matrix[2][1], matrix[2][2], matrix[2][3]);
+    printf("%X %X %X %X \n\n", matrix[3][0], matrix[3][1], matrix[3][2], matrix[3][3]);
 }
 
 
-int encrypt(){
+int encrypt() {
     createKeySchedule();
 
-    AddRoundKey();
+    AddRoundKey(0);
 
-    for(int i = 0; i<9;i++){
+    for (int i = 1; i < 10; i++) {
         SubBytes();
+
+        ShiftRows();
+        MixColumns();
+        AddRoundKey(i);
+
 
     }
 
+    //Last Round
+    SubBytes();
+    ShiftRows();
+    AddRoundKey(10);
 
     return 0;
 }
 
 int main() {
-    createKeySchedule();
-    printTestSchedule();
-    /*SubBytes();
+    encrypt();
     printTest();
-    ShiftRows();
-    printTest();
-    MixColumns();
-    AddRoundKey();
-    printTest();*/
     return 0;
 }
